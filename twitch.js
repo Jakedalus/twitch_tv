f71d18b2981a8ccab670b5ad6d2bb2a98a7003fd
Jakedalus <jacquedalus@gmail.com>
2017-05-28 21:40:35 -0400

    Add autocomplete search function. Some bugs to work out still

--- twitch.js
+++ twitch.js
@@ -4,6 +4,68 @@ var endpoint = "https://wind-bow.glitch.me/twitch-api/";
 var usernames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "brunofin", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
 var $table = $("tbody");
+var $search = $("#search");
+var list = [];
+
+$search.autocomplete({
+    source: usernames,
+    response: function(e, ui) {
+        
+        list = ui.content;
+//        console.log(e);
+//        console.log(ui);
+        var i = 0;
+        $("tr").show();
+        $("tr").hide();
+        
+        list.forEach(function() {
+            var val = list[i].value.toLocaleLowerCase();
+            console.log("#" + val);
+            console.log($("#" + val));
+            $("#" + val).parent().show(); 
+            i++;
+        });
+        $('#myTabs a').trigger("click");
+    },
+    select: function(e, ui) {
+        
+        var val = ui.item.value.toLocaleLowerCase();
+        console.log(e);
+        console.log($("td#" + val));
+        $("tr").show();
+        $("tr").hide();
+        $("#" + val).parent().show();
+        $('#myTabs a').trigger("click");
+        
+    },
+//    close: function(e, ui) {
+//        console.log(e);
+//        var i = 0;
+//        $("tr").show();
+//        $("tr").hide();
+//        list.forEach(function() {
+//            console.log("#" + list[i].value);
+//            console.log($("#" + list[i].value));
+//            $("#" + list[i].value).parent().show(); 
+//            i++;
+//        });
+//    }
+});
+
+$search.on("keypress keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $('#myTabs a').trigger("click");
+    
+    if(value == "") {
+        $("tr").show();
+    } 
+    $('#myTabs a').trigger("click");
+//    else {
+//        $("tr").hide();
+//    }
+});
+
+
 
 $('#myTabs a').click(function (e) {
     e.preventDefault();
@@ -51,8 +113,22 @@ for(var i = 0; i < usernames.length; i++) {
         console.log(d2[0].stream);
         console.log(" ");
         
+        
+        var name = d1[0].display_name;
+        
+        if(name == undefined) {
+            var msg = d1[0].message;
+            console.log(msg.indexOf("\""));
+            var name = msg.slice(msg.indexOf("\"") + 1, msg.lastIndexOf("\""));
+        }
+        
+        
+        name = name.toLocaleLowerCase();
+        
+        console.log("Name: ", name);
+        
         $row.append($("<td></td>").html(d3[0].logo ? "<img src=" + d3[0].logo +">" : "n/a"));
-        $row.append($("<td></td>").html(d1[0].display_name ? "<a href='" + twichURL + UN + "'>" + d1[0].display_name + "</a>" : "<i>" + d1[0].message + "</i>"));
+        $row.append($("<td></td>").html(d1[0].display_name ? "<a href='" + twichURL + UN + "'>" + d1[0].display_name + "</a>" : "<i>" + d1[0].message + "</i>").attr("id", name));
         $row.append($("<td></td>").html(d1[0].bio ? d1[0].bio : "<i>No bio.</i>"));
         $row.append($("<td></td>").html(d2[0].stream ? d2[0].stream.channel.status : "No stream."));
         
